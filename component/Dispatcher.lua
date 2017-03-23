24e2885007f5a3ab20c5fa6a82225f8294972f68
yangqi <yangqi@3pjgames.com>
2017-03-23 23:24:51 +0800

    增加一个Dispatcher

---
+++ component/Dispatcher.lua
@@ -0,0 +1,34 @@
+local Dispatcher = {}
+
+function Dispatcher.create()
+    local ret = {}
+    setmetatable(ret, {__index=Dispatcher})
+    ret:init()
+    return ret
+end
+
+function Dispatcher:init()
+    self._subscribers = {}
+    self._messageQueue = {}
+end
+
+function Dispatcher:addSubscriber(subscriber)
+    table.insert(self._subscribers, subscriber)
+end
+
+function Dispatcher:removeSubscriber(subscriberId)
+    for i,v in ipairs(subscriberId) do
+        if v.id==subscriberId then
+            table.remove(self._subscribers, i)
+            break
+        end
+    end
+end
+
+function Dispatcher:dispatch(msg)
+    for _,subscriber in ipairs(self._subscribers) do
+        subscriber:receive(msg)
+    end
+end
+
+return Dispatcher
